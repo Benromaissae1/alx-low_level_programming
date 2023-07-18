618616ffedb73e276fd03d6c2284d883fd83d614
benromaissae1 <benromaissae1@gmail.com>
2023-07-18 12:27:15 -0700

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,6 +4,9 @@
  * print_sign - print + if n is greater than zero,
  *                    0 if n is zero and - if n is
  *                    less than zero
+ *
+ * @n: takes integre type input for function
+ *
  * Return: 1 if +, 0 if 0  and -1 if -
  */
 
