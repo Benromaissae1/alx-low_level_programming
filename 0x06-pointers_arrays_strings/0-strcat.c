45269784ea614b54f467d11009787a5b5198cecc
benromaissae1 <benromaissae1@gmail.com>
2023-07-27 10:50:18 -0700

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * strcat - function that concatenates two strings.
+ * _strcat - function that concatenates two strings.
  *
  * @dest: pointer to destnation input
  * @src: pointer to source input
