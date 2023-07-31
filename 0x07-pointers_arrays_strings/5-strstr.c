b3ec5888fc75af3464cd5da7ae5e40a7c89229d1
benromaissae1 <benromaissae1@gmail.com>
2023-07-31 07:33:03 -0700

    task 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _strstr - function that locates a substring.
+ * @needle: substring
+ * @haystack: string
+ * Return: pointer to needle
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+
