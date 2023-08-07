a9f9421e00931d3b793e14e93ea470215ef4bd3b
benromaissae1 <benromaissae1@gmail.com>
2023-08-07 02:44:50 -0700

    task 3

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -4,4 +4,6 @@
 int main(int argc __attribute__((unused)), char *argv[]);
 int main(int argc, char *argv[] __attribute__((unused)));
 int main(int argc, char *argv[]);
+int _atoi(char *s);
+int main(int argc, char *argv[]);
 #endif 
