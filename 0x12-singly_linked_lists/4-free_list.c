5a957a965bc23205c1081886196caa40dabe3973
benromaissae1 <benromaissae1@gmai.com>
2023-08-24 18:16:01 -0700

    4. Free list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -11,12 +11,9 @@ void free_list(list_t *head)
 {
 	if (head)
 	{
-		free_list(head->next);
-		free(head);
-	}
+	free_list(head->next);
 	if (head->str)
-	{
-		free(head->str);
+			free(head->str);
 		free(head);
 	}
 }
