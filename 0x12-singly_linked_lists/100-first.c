734b810deb1e23b3888d9aae78c6ff0ff3a8cfc9
benromaissae1 <benromaissae1@gmai.com>
2023-08-25 14:47:00 -0700

    uploading first 100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * myprintf - printf some sentences
+ */
+
+void myprintf(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
